a6f5fafead667ebcfd4103e3cdf730a6a4b7b0c9
ZC <zhoucan1999@outlook.com>
2020-04-04 03:30:28 +0100

    (faster 4 times) Finish initialization of 3d matrix by using SSE

---
+++ FOP/helloWorld.cc
@@ -0,0 +1,7 @@
+#ifdef __INTEL_COMPILER
+  // Only compiled with Intel Compiler
+  printf("Hello world from Intel compiler");
+#elif __GNUC__
+  // Only compiled with GNU Compiler
+  printf("Hello world from GNU compiler");
+#endif
